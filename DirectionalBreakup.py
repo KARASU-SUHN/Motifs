e6cd3ca06474203dfe9ed866bc7e7c7b3d9ac7d7
KARASU-SUHN <43245579+KARASU-SUHN@users.noreply.github.com>
2023-07-12 21:57:20 +0900

    Create DirectionalBreakup.py

---
+++ DirectionalBreakup.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.sparse import csc_matrix
+
+def directional_breakup(A):
+    # Set non-zero elements to 1
+    A[A != 0] = 1
+    
+    # Bidirectional subgraph
+    B = csc_matrix((A.multiply(A.T) != 0).astype(int))
+    
+    # Unidirectional subgraph
+    U = A - B
+    
+    # Undirected graph
+    G = A | A.T
+    
+    return B, U, G
+
+# % DIRECTIONALBREAKUP returns the bidirectional, unidirectional, and
+# % undirected versions of the adjacency matrix A.
+# %
+# % [B, U, G] = DirectionalBreakup(A) returns
+# %   B: the bidirectional subgraph
+# %   U: the unidirectional subgraph
+# %   G: the undirected graph
+# %
+# %  Note that G = B + U
