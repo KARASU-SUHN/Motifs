9e98bc6de7a22c0424456097d8e7d3063ede3e01
KARASU-SUHN <43245579+KARASU-SUHN@users.noreply.github.com>
2023-07-12 22:18:44 +0900

    Create SpectralPartitioning.py

---
+++ SpectralPartitioning.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def SpectralPartitioning(A):
+    part_vec, _ = nfiedler(A)
+    order = np.argsort(part_vec)
+
+    B = A[order, :][:, order]
+    B_lower = np.tril(B)
+    B_sums = np.sum(B, axis=1)
+    B_lower_sums = np.sum(B_lower, axis=1)
+    volumes = np.cumsum(B_sums)
+    num_cut = np.cumsum(B_sums - 2 * B_lower_sums)
+    total_vol = np.sum(A)
+    volumes_other = total_vol * np.ones(len(order)) - volumes
+    vols = np.minimum(volumes, volumes_other)
+    scores = num_cut / vols
+    scores = scores[:-1]
+
+    condc, min_ind = np.min(scores), np.argmin(scores)
+
+    n = A.shape[0]
+    if min_ind <= np.floor(n / 2):
+        cluster = order[:min_ind]
+    else:
+        cluster = order[min_ind + 1:]
+
+    condv = np.minimum(scores, scores[::-1])
+    condv = condv[:int(np.ceil(len(scores) / 2))]
+
+    return cluster, condv, condc, order
+
+# % SPECTRALPARTITIONING performs a spectral partitioning of A and
+# % returns several relevant quantities.  It assumes that A is undirected
+# % and connected.
+# %
+# %  [order, condv, comm, condc] = SpectralPartitioning(A) returns
+# %   cluster: vector of nodes in the smaller side of the partition
+# %   condv: the sweep conductance vector
+# %   condc: the conductance of the cluster
+# %   order: the spectral ordering
